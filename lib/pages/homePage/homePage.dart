a8224f31698cdb2c0c1590e404a7c1d8f7918c37
RohitRajP <RohitRajP>
2020-08-23 09:41:51 +0530

    Finished googleSignIn

--- lib/pages/homePage/homePage.dart
+++ lib/pages/homePage/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:freemedium/imports.dart';
 
 /// class to hold the homePage
@@ -19,7 +20,13 @@ class _HomePageState extends State<HomePage> {
   Widget _homePageBody() {
     return SafeArea(
       child: Container(
-        child: null,
+        child: InAppWebView(
+          initialUrl: "https://medium.com",
+          initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(
+                  userAgent:
+                      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36")),
+        ),
       ),
     );
   }
