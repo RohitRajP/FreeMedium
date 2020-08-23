e64419941000971939bba8598ba6fe62f112a7b8
RohitRajP <RohitRajP>
2020-08-23 08:52:23 +0530

    Initial app setup

---
+++ lib/imports.dart
@@ -0,0 +1,16 @@
+// package imports
+export 'package:flutter/material.dart';
+export 'package:provider/provider.dart';
+
+// model imports
+
+// page imports
+
+// global imports
+export 'package:freemedium/global/globalData.dart';
+export 'package:freemedium/global/globalFunctions.dart';
+export 'package:freemedium/global/globalTheme.dart';
+export 'package:freemedium/global/globalWidgets.dart';
+export 'package:freemedium/global/globalScaffoldKeys.dart';
+
+// service imports
